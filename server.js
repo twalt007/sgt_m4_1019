1de1ecae3fcdaef2ec24cbbfc186e230a7da128b
twalt007 <tatumwalter@gmail.com>
2019-11-23 17:57:07 -0800

    USed ENV variable for PORT number

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('./db');
 const app = express();
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -100,8 +101,8 @@ app.post('/api/students',async(req,res) => {
 
 })
 
-app.listen(3000, () => {
-    console.log("Server is listening @ localhost:3000")
+app.listen(PORT, () => {
+    console.log("Server is listening @ localhost: " + PORT)
 });
 
 
